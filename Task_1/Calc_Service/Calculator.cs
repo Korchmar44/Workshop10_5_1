4ab222339dfd83a82b40a94dfcf4af53ff9c3b62
Денис Голубев <d.golubev@corp.local>
2024-03-12 15:50:09 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Calc_Service/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_1.Interfaces;
+using Task_1.Log;
+
+namespace Task_1.Calc_Service
+{
+    public class Calculator : ICalculator
+    {
+        ILogger Logger { get; }
+        public Calculator(ILogger logger)
+        {
+            Logger = logger;
+        }
+        public double Add(double num1, double num2)
+        {
+            Logger.Event("Выполняется операция сложения");
+            return num1 + num2;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                Console.WriteLine("Введите аргумент 1");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите аргумент 2");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                var resalt = Add(num1, num2);
+
+                Console.WriteLine($"Сумма чисел: {num1} и {num2} равна {resalt}");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_1.Calc_Service;
+using Task_1.Interfaces;
+using Task_1.Log;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+
+            var calculator = new Calculator(Logger);
+
+            calculator.Run();
+        }
+    }
+}
